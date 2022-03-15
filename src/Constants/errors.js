fce30db174bfc9994590697862b5ccc9c07b38cc
Sonika Gupta <gupta.sonika103@gmail.com>
2022-03-15 22:06:18 +0530

    Add error messages

---
+++ src/Constants/errors.js
@@ -0,0 +1,9 @@
+module.exports = {
+  invalidInput: 'Invalid Input',
+  invalidBowlType: 'Invalid Bowl Type',
+  invalidShotType: 'Invalid Shot Type',
+  invalidShotTiming: 'Invalid Shot Timing',
+  unmatchedTypeTeam: 'Team Type Not Matched',
+  teamAlreadyAssigned: 'Teams Already Assigned',
+  requiredSixEntries: 'Need Six Shots for an Over'
+}
