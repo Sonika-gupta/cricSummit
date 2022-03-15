844173a8c3df0848e085f4d92d916b06d00cd94b
Sonika Gupta <gupta.sonika103@gmail.com>
2022-03-15 22:04:25 +0530

    Add Data Files

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+const bowlTypes = require('./bowlTypes')
+const shotTypes = require('./shotTypes')
+const shotTimings = require('./shotTimings')
+const teams = require('./teams')
+const outcomes = require('./outcomes')
+
+module.exports = {
+  bowlTypes,
+  shotTypes,
+  shotTimings,
+  teams,
+  outcomes
+}
